4d997e0997303767d6b783d0d2b469898991ec41
Jeongeun Lee <leeju1009@gmail.com>
2023-05-19 16:42:30 +0900

    :sparkles: 칵테일 이미지 및 칵테일명 클릭 시, 상세 페이지로 이동하는 기능 추가

--- src/components/Category/CocktailList.tsx
+++ src/components/Category/CocktailList.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 interface cocktailListType {
   [key: string]: string;
@@ -14,32 +15,41 @@ function CocktailList({
   isChecked,
 }: cocktailListPropsType) {
   const [cocktailList, setCocktailList] = useState<cocktailListType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   useEffect(() => {
+    setIsLoading(() => true);
     const newCocktailList = alcoholFilteredList.filter(
       (cocktail) =>
         isChecked.includes(cocktail.strCategory) ||
         isChecked.includes(cocktail.strIngredient1)
     );
     setCocktailList(() => newCocktailList);
+    setIsLoading(() => false);
   }, [alcoholFilteredList, isChecked]);
 
   return (
     <>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {isLoading && <div>칵테일 목록 받아오는 중..</div>}
         {cocktailList.length !== 0
           ? cocktailList.map((cocktail, idx) => (
               <div key={idx}>
-                <img
-                  src={cocktail.strDrinkThumb}
-                  alt={cocktail.strDrink}
-                  style={{
-                    margin: 0,
-                    width: 250,
-                    height: 250,
-                    display: 'flex',
-                  }}
-                />
-                <p>{cocktail.strDrink}</p>
+                <Link to={`/detail/${cocktail.idDrink}`}>
+                  <img
+                    src={cocktail.strDrinkThumb}
+                    alt={cocktail.strDrink}
+                    style={{
+                      margin: 0,
+                      width: 250,
+                      height: 250,
+                      display: 'flex',
+                    }}
+                  />
+                </Link>
+                <Link to={`/detail/${cocktail.idDrink}`}>
+                  <p>{cocktail.strDrink}</p>
+                </Link>
               </div>
             ))
           : '설정된 카테고리에 해당하는 칵테일이 없습니다.'}
