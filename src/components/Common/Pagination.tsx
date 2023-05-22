696918034cb08f6a93170e4f4b640663baec2205
Jeongeun Lee <leeju1009@gmail.com>
2023-05-23 01:40:45 +0900

    :building_construction: Pagination 컴포넌트를 공용 컴포넌트 파일로 이동

---
+++ src/components/Common/Pagination.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Pagination.module.scss';
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  handlePageQueryChange: (targetPageNumber: number) => void;
+}
+
+function Pagination({
+  totalPages,
+  currentPage,
+  handlePageQueryChange,
+}: PaginationProps) {
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1
+  );
+
+  //디버깅
+  useEffect(() => {
+    console.log('현재페이지', currentPage);
+  }, [currentPage]);
+
+  const handlePreviousPageClick = () => {
+    if (currentPage > 1) {
+      handlePageQueryChange(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    handlePageQueryChange(pageNumber);
+  };
+
+  const handleNextPageClick = () => {
+    if (currentPage < totalPages) {
+      handlePageQueryChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className={styles.paginationBar}>
+      <button onClick={handlePreviousPageClick}>이전</button>
+      {pageNumbers.map((pageNumber) =>
+        currentPage === pageNumber ? (
+          <span key={pageNumber} className={styles.currentPageNumber}>
+            {pageNumber}
+          </span>
+        ) : (
+          <NavLink
+            to={`?page=${pageNumber}`}
+            className={styles.pageNumber}
+            key={pageNumber}
+            onClick={() => handlePageClick(pageNumber)}
+          >
+            {pageNumber}
+          </NavLink>
+        )
+      )}
+      <button onClick={handleNextPageClick}>다음</button>
+    </div>
+  );
+}
+
+export default Pagination;
