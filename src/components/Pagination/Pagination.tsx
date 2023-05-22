f3b44b5a9b58444d2e44c7d82a8a58e1077ec0e7
junhui324 <junhuijang00@gmail.com>
2023-05-22 17:00:29 +0900

    :sparkles: detail 페이지 이동 및 페이지네이션 추가
    CSS 추가
    1. CWLayout 새로 만듦
    2. 요소 배치 다수 변경

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Pagination.module.scss';
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  handlePageQueryChange: (targetPageNumber: number) => void;
+}
+
+const activeStyle = {
+  background: 'grey',
+  color: 'white',
+};
+
+function Pagination({
+  totalPages,
+  currentPage,
+  handlePageQueryChange,
+}: PaginationProps) {
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1
+  );
+
+  //디버깅
+  useEffect(() => {
+    console.log('현재페이지', currentPage);
+  }, [currentPage]);
+
+  const handlePreviousPageClick = () => {
+    if (currentPage > 1) {
+      handlePageQueryChange(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    handlePageQueryChange(pageNumber);
+  };
+
+  const handleNextPageClick = () => {
+    if (currentPage < totalPages) {
+      handlePageQueryChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className={styles.paginationBar}>
+      <button onClick={handlePreviousPageClick}>이전</button>
+      {pageNumbers.map((pageNumber) =>
+        currentPage === pageNumber ? (
+          <span className={styles.activeLink} key={pageNumber}>
+            {pageNumber}
+          </span>
+        ) : (
+          <NavLink
+            className={styles.link}
+            to={`?page=${pageNumber}`}
+            key={pageNumber}
+            onClick={() => handlePageClick(pageNumber)}
+          >
+            {pageNumber}
+          </NavLink>
+        )
+      )}
+      <button onClick={handleNextPageClick}>다음</button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import Layout from '../Layout/Layout';
-import { SearchAddress } from '../components/Weather/SearchAddress';
-import { GetWeather } from '../components/Weather/GetWeather';
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
+import React from "react";
+import Layout from "../Layout/Layout";
+import { SearchAddress } from "../components/Weather/SearchAddress";
+import { GetWeather } from "../components/Weather/GetWeather";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
+import CWLayout from "../Layout/CWLayout";
 
 function Weather() {
   return (
     <Provider store={store}>
-      <div>
-        <Layout>
-          <GetWeather></GetWeather>
-          <SearchAddress></SearchAddress>
-        </Layout>
-      </div>
+      <CWLayout>
+        <GetWeather></GetWeather>
+      </CWLayout>
     </Provider>
   );
 }
