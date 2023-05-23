18c8a4cfc3fc152dd61416f919747a93ab4f859b
hyunjung <paintover2023@gmail.com>
2023-05-24 01:59:17 +0900

    :memo: landing 페이지 업데이트

--- src/components/Landing/CocktailMatch.tsx
+++ src/components/Landing/CocktailMatch.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-import CocktailList from './WeatherToGlass';
-import styles from "./CocktailMatch.module.scss"
+import CocktailList from "./WeatherToGlass";
+import styles from "./CocktailMatch.module.scss";
 import { useNavigate } from "react-router-dom";
-import { fetchWeatherData, getLocation } from '../../API/WeatherAPI/index';
+import { fetchWeatherData, getLocation } from "../../API/WeatherAPI/index";
 
 function CocktailMatch() {
   const navigate = useNavigate();
-  
+
   // 추천받은 칵테일 상세 페이지로 넘어가기
-  const [selectedCocktailId, setSelectedCocktailId] = useState<string | null>(null);
-  console.log('랜덤 칵테일 id:', selectedCocktailId)
+  const [selectedCocktailId, setSelectedCocktailId] = useState<string | null>(
+    null
+  );
+  console.log("랜덤 칵테일 id:", selectedCocktailId);
 
   const handleButtonClickForDetail = () => {
     if (selectedCocktailId) {
@@ -19,21 +21,16 @@ function CocktailMatch() {
     }
   };
 
-  // weather 페이지로 넘어가기  
+  // weather 페이지로 넘어가기
   const handleLocationBtnClick = () => {
     navigate(`/weather`);
-  }
+  };
 
-
-    
-
-  
-  
   // 칵테일 다시 추천받기 기능
-  const [reloadKey, setReloadKey] = useState(0); 
+  const [reloadKey, setReloadKey] = useState(0);
   const handleButtonClickForReload = () => {
-    setReloadKey((prevKey) => prevKey + 1); 
-  }
+    setReloadKey((prevKey) => prevKey + 1);
+  };
 
   // weather 데이터 불러오기
   type WeatherData = {
@@ -44,7 +41,7 @@ function CocktailMatch() {
     weather: {
       main: any;
       description: string;
-      icon: string,
+      icon: string;
     }[];
   };
 
@@ -59,7 +56,7 @@ function CocktailMatch() {
         const weatherData = await fetchWeatherData(latitude, longitude);
         setWeather(weatherData as WeatherData);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
 
@@ -67,7 +64,9 @@ function CocktailMatch() {
   }, []);
 
   if (!weather) {
-    return <div className={styles.loadingPg}>날씨에 어울리는 칵테일 제조중...</div>;
+    return (
+      <div className={styles.loadingPg}>날씨에 어울리는 칵테일 제조중...</div>
+    );
   }
 
   const { name, main, weather: weatherDetails } = weather;
@@ -77,36 +76,56 @@ function CocktailMatch() {
   const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
 
   return (
-      <div>
-        <h3 className={styles.h3}>현재 위치한 곳의 날씨</h3>
-        <h3 className={styles.h3}>{name}</h3>
-        <button
-          onClick={handleLocationBtnClick} 
-          className={styles.locationBtn}>주소별 칵테일 조회하러가기</button>
-        <p className={styles.p}>날씨 : {description} | {temperature} </p>
-        
-        <div className={styles.weatherIcons}>
-          <img src={iconUrl} alt="Weather Icon" className={styles.weatherIcon}></img>
-          <img src={iconUrl} alt="Weather Icon" className={styles.weatherIcon}></img>
-          <img src={iconUrl} alt="Weather Icon" className={styles.weatherIcon}></img>
-        </div>
-        <h3 className={styles.h3}>[{description}] 날씨에 어울리는 칵테일</h3>
- 
-        <CocktailList 
-          weather={weatherName}
-          key={reloadKey}
-          onCocktailSelected={setSelectedCocktailId}
-          /> 
+    <div>
+      <h3 className={styles.h3}>현재 위치한 곳의 날씨</h3>
+      <h3 className={styles.h3}>{name}</h3>
+      <button onClick={handleLocationBtnClick} className={styles.locationBtn}>
+        주소별 칵테일 조회하러가기
+      </button>
+      <p className={styles.p}>
+        날씨 : {description} | {temperature}{" "}
+      </p>
 
-        <div className={styles.buttonBox}>
-          <button 
-            onClick={handleButtonClickForDetail}
-            className={styles.learnBtn}>제조법 알아보기</button>
-          <button 
-            onClick={handleButtonClickForReload}
-            className={styles.againBtn}>다시 추천받기</button>
-        </div>
+      <div className={styles.weatherIcons}>
+        <img
+          src={iconUrl}
+          alt="Weather Icon"
+          className={styles.weatherIcon}
+        ></img>
+        <img
+          src={iconUrl}
+          alt="Weather Icon"
+          className={styles.weatherIcon}
+        ></img>
+        <img
+          src={iconUrl}
+          alt="Weather Icon"
+          className={styles.weatherIcon}
+        ></img>
       </div>
+      <h3 className={styles.h3}>[{description}] 날씨에 어울리는 칵테일</h3>
+
+      <CocktailList
+        weather={weatherName}
+        key={reloadKey}
+        onCocktailSelected={setSelectedCocktailId}
+      />
+
+      <div className={styles.buttonBox}>
+        <button
+          onClick={handleButtonClickForDetail}
+          className={styles.learnBtn}
+        >
+          제조법 알아보기
+        </button>
+        <button
+          onClick={handleButtonClickForReload}
+          className={styles.againBtn}
+        >
+          다시 추천받기
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Landing/WeatherToGlass.tsx
@@ -0,0 +1,86 @@
+// 날씨와 매칭된 글래스옵션 api를 호출
+// 호출된 API에서 반환된 칵테일 중에서 랜덤으로 하나를 선택하여 추천하는 기능을 추가
+
+import React, { useEffect, useState } from 'react';
+import styles from './Landing.module.scss'
+
+const weatherToGlass = {
+  'Thunderstorm': 'Coffee_mug',
+  'Tornado': 'Coffee_mug',
+  'Rain': 'Coffee_mug',
+  'Squall': 'Coffee_mug',
+  'Drizzle': 'Collins_glass',
+  'Mist': 'Collins_glass',
+  'Smoke': 'Collins_glass',
+  'Fog': 'Collins_glass',
+  'Haze': 'Collins_glass',
+  'Snow': 'Irish_coffee_cup',
+  'Clear': 'Cocktail_glass',
+  'Clouds': 'Highball_glass',
+  'Dust': 'Highball_glass',
+  'Sand': 'Highball_glass',
+  'Ash': 'Highball_glass'
+};
+
+interface Cocktail {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+}
+
+interface Props {
+  weather: keyof typeof weatherToGlass;
+  onCocktailSelected?: (id: string) => void;
+}
+
+const CocktailList: React.FC<Props> =({ weather, onCocktailSelected }) => {
+  const [cocktail, setCocktail] = useState<Cocktail | null>(null);
+
+  useEffect(() => {
+    const fetchCocktails = async () => {
+      const glassOption = weatherToGlass[weather];
+      if (!glassOption) {
+        throw new Error(`Invalid weather type: ${weather}`);
+      }
+
+      try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glassOption}`);
+        const data = await response.json();
+        const cocktails = data.drinks;
+        const randomIndex = Math.floor(Math.random() * cocktails.length);
+        const selectedCocktail = cocktails[randomIndex]; // 랜덤으로 선택된 칵테일
+
+        setCocktail({
+          idDrink: selectedCocktail.idDrink, 
+          strDrink: selectedCocktail.strDrink, 
+          strDrinkThumb: selectedCocktail.strDrinkThumb, 
+        });
+        
+        // 선택된 칵테일의 ID를 상위 컴포넌트에 전달
+        if (typeof onCocktailSelected === 'function') {
+          onCocktailSelected(selectedCocktail.idDrink);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchCocktails();
+  }, [weather, onCocktailSelected]);
+
+  if (!cocktail) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h4 className={styles.cocktailName}>{cocktail.strDrink}</h4>
+      <img 
+        src={cocktail.strDrinkThumb} 
+        alt={cocktail.strDrink} 
+        className={styles.drinkImg}
+        />
+    </div>
+  );
+};
+
+export default CocktailList;
