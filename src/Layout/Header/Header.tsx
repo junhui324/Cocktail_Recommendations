d1a81fc4bac69abbd91d23e18ca129c01f1b3069
Rki0 <pky00823@gmail.com>
2023-05-25 11:05:32 +0900

    ⚡: 반응형 헤더 성능 개선
    어떤 위치에서든 사이드 메뉴를 열고, 닫을 수 있으며
    브라우저 크기 변경 시, 이를 탐지하여 열려있는 사이드 메뉴를 자동 닫힘 처리합니다.
    또한, 스크롤을 탐지하여 위로 향할 때 네비게이션이 보이고, 아래로 향할 때 네비게이션이 가려집니다.

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Logo from "./Logo";
 import NavBar from "./NavBar";
@@ -8,20 +8,58 @@ import styles from "./Header.module.scss";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentScroll, setCurrentScroll] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  // HamburgerMenu가 보이는 상태에서 화면 크기가 반응형 헤더를 변경 시킬 시
+  // 토글을 통해 HamburgerMenu를 닫는다.
+  useEffect(() => {
+    const checkWindow = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", checkWindow);
+
+    return () => {
+      window.removeEventListener("resize", checkWindow);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < 45) {
+        return;
+      }
+
+      const moving = window.pageYOffset;
+
+      setVisible(currentScroll > moving);
+
+      setCurrentScroll(moving);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [currentScroll, isOpen]);
 
   const clickHandler = () => {
     setIsOpen((prev) => !prev);
   };
 
   return (
-    <header className={styles.header}>
-      {/* <Logo /> */}
+    <header className={`${styles.header} ${visible ? styles.scrolled : ""}`}>
+      <Logo />
 
       <NavBar />
 
       <Hamburger clickHandler={clickHandler} />
 
-      {isOpen && <HamburgerMenu clickHandler={clickHandler} />}
+      <HamburgerMenu clickHandler={clickHandler} isOpen={isOpen} />
     </header>
   );
 }
--- src/Layout/Header/Logo.tsx
+++ src/Layout/Header/Logo.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import styles from "./Logo.module.scss";
 
 function Logo() {
   return (
-    <div>
-      <img alt="logo" src="" />
-    </div>
+    <Link to="/" className={styles.logo_link}>
+      <img alt="logo" src={logo} className={styles.logo_img} />
+    </Link>
   );
 }
 
