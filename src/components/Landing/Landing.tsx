6254725c27b77caf88d572aa7d220818e44dd591
hyunjung <paintover2023@gmail.com>
2023-05-26 09:05:41 +0900

    업데이트

---
+++ src/components/Landing/Landing.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CocktailMatch from '../components/Landing/CocktailMatch';
+import CHLayout from "../components/Landing/CHLayout";
+import CHPageCard from '../components/Landing/CHPageCard';
+
+
+function Landing() {
+  return (
+    <CHLayout>
+      <CHPageCard>
+      <CocktailMatch></CocktailMatch>
+      </CHPageCard>
+    </CHLayout>
+  );
+}
+
+export default Landing;
